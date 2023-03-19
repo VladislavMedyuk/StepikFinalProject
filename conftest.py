e681823fa7f94ebeb764c217dbd5c25df86bf6f8
Vladislav Medyuk <vlad.medyuk.02@mail.ru>
2023-03-19 23:00:21 +0300

    add browser selection

--- conftest.py
+++ conftest.py
@@ -6,10 +6,25 @@ from selenium.webdriver.chrome.options import Options
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default=None,
                      help="Choose language")
+    parser.addoption('--browser_name', action='store', default=None,
+                     help="Choose chrome or firefox")
+
+
 @pytest.fixture(scope="function")
 def browser(request):
     # В переменную user_language передается параметр из командной строки
     user_language = request.config.getoption("language")
+
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome()
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox()
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
     # Инициализируются опции браузера
     options = Options()
     # В опции вебдрайвера передаем параметр из командной строки
