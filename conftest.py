0bb6c954116b7fbbe1b8fe49eabaf3b0ebaa9826
Vladislav Medyuk <vlad.medyuk.02@mail.ru>
2023-03-23 14:51:03 +0300

    bugs fix

--- conftest.py
+++ conftest.py
@@ -1,37 +1,36 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver import FirefoxOptions
 
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default=None,
-                     help="Choose language")
-    parser.addoption('--browser_name', action='store', default=None,
-                     help="Choose chrome or firefox")
+    parser.addoption('--browser_name', action='store', default="chrome",
+                     help="Choose browser: chrome or firefox")
+
+    parser.addoption('--language', action='store', default='en', help='Choose language')
 
 
 @pytest.fixture(scope="function")
 def browser(request):
-    # В переменную user_language передается параметр из командной строки
-    user_language = request.config.getoption("language")
-
     browser_name = request.config.getoption("browser_name")
+    language = request.config.getoption("language")
     browser = None
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        browser = webdriver.Chrome(options=options)
+        browser.maximize_window()
+        browser.implicitly_wait(5)
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
-    else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
-    # Инициализируются опции браузера
-    options = Options()
-    # В опции вебдрайвера передаем параметр из командной строки
-    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-    browser = webdriver.Chrome(options=options)
-
-    browser.implicitly_wait(5)
+        options = FirefoxOptions()
+        options.set_preference("intl.accept_languages", language)
+        browser = webdriver.Firefox(options=options)
+        browser.maximize_window()
+        browser.implicitly_wait(5)
     yield browser
     print("\nquit browser..")
     browser.quit()
