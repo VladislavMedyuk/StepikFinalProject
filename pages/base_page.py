22abdfd147be915ee1f9e9febe274062c7b85f26
Vladislav Medyuk <vlad.medyuk.02@mail.ru>
2023-03-24 22:03:00 +0300

    refactor

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,6 @@
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.wait import WebDriverWait
-from resourses.time import Timeout
+from resourses.env import Resources
 from selenium.webdriver.support import expected_conditions as EC
 
 
@@ -30,7 +30,7 @@ class BasePage:
 
     def is_element_present(self, method, selector):
         try:
-            WebDriverWait(self.browser, Timeout.timeout).until(
+            WebDriverWait(self.browser, Resources.timeout).until(
                 EC.presence_of_element_located((method, selector))
             )
         except NoSuchElementException:
