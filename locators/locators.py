f5295069af3929ec93560796db3af2e7d6cae89b
Vladislav Medyuk <vlad.medyuk.02@mail.ru>
2023-03-27 23:48:18 +0300

    add locators for product page

--- locators/locators.py
+++ locators/locators.py
@@ -16,3 +16,12 @@ class LoginPageLocators:
     PASSWORD = (By.NAME, "registration-password1")
     REPEAT_PASSWORD = (By.NAME, "registration-password2")
     REGISTRATION_BUTTON = (By.NAME, "registration_submit")
+
+
+class ProductPageLocators:
+    BOOK_NAME = (By.CSS_SELECTOR, "#messages div:nth-child(1) strong")
+    BOOK_PRICE = (By.CSS_SELECTOR, "#messages div:nth-child(3) strong")
+    ADD_TO_BASKET_BUTTON = (By.CLASS_NAME, "btn-add-to-basket")
+    BOOK_TO_COMPARE = (By.CSS_SELECTOR, ".product_main h1")
+    PRICE_TO_COMPARE = (By.CSS_SELECTOR, ".product_main p.price_color")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages div:nth-child(1) .alertinner")
