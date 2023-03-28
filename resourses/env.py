f7a23785281fb234993a4bd4e0a0b4fb3cf563f6
Vladislav Medyuk <vlad.medyuk.02@mail.ru>
2023-03-28 16:40:56 +0300

    add list of links for finding bug

--- resourses/env.py
+++ resourses/env.py
@@ -1,10 +1,22 @@
 import enum
+import pytest
 
 
 class Resources:
-    PRODUCT_PAGE_LINK = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    PRODUCT_PAGE_LINK = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     MAIN_LINK = "http://selenium1py.pythonanywhere.com/"
     TIMEOUT = 30
+    LINKS = ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+             "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+             "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+             "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+             "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+             "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+             "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+             pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                          marks=pytest.mark.xfail),
+             "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+             "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"]
 
 
 class Browsers(enum.Enum):
