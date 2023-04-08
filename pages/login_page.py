4893d8fc2ad92da2a8c1b51cbd33e363c74aa4eb
Vladislav Medyuk <vlad.medyuk.02@mail.ru>
2023-04-08 20:08:43 +0300

    add new methods

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,8 +1,14 @@
 from pages.base_page import BasePage
 from locators.locators import LoginPageLocators
+from faker import Faker
 
 
 class LoginPage(BasePage):
+    @staticmethod
+    def make_email_and_password() -> tuple:
+        faker = Faker()
+        return faker.email(), faker.password()
+
     def should_be_login_page(self) -> None:
         self.should_be_login_url()
         self.should_be_login_form()
@@ -16,3 +22,15 @@ class LoginPage(BasePage):
 
     def should_be_register_form(self) -> None:
         assert self.is_element_present(LoginPageLocators.REGISTRATION_FORM), "Register form is not present"
+
+    def register_new_user(self, email, password):
+        email_input = self.search_element(LoginPageLocators.REGISTRATION_EMAIL)
+        password_input = self.search_element(LoginPageLocators.REGISTRATION_PASSWORD)
+        repeat_password = self.search_element(LoginPageLocators.REGISTRATION_REPEAT_PASSWORD)
+        submit_button = self.search_element(LoginPageLocators.REGISTRATION_BUTTON)
+
+        email_input.send_keys(email)
+        password_input.send_keys(password)
+        repeat_password.send_keys(password)
+
+        submit_button.click()
